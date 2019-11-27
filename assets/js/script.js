11c5952d12e8c389a3c8d6b6598032c877093ec2
DinaTaklit <ez_taklit@esi.dz>
2019-11-27 10:26:08 +0100

    feat: Add the function that show the name letter by letter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
 /*******************  Satrt Home Script *********************/
+/** Start Show the name Script **/
+function printLetterByLetter(destination, message, speed) {
+    var i = 0;
+    var interval = setInterval(function() {
+        destination.innerHTML += message.charAt(i);
+        i++;
+        if (i > message.length) {
+            clearInterval(interval);
+        }
+    }, speed);
+}
+var spanName = document.querySelector("#Intro span.name");
+printLetterByLetter(spanName, "Dina TAKLIT", 100);
+/** END Show the name Script **/
+
 /** Start Btn more Script **/
 // Get the view more buttom
 var viewMoreBtn = document.querySelector("#view-more");
-console.log(viewMoreBtn);
 // Once the user hover remove the arrow-right class and replace it with arrow-down one
 viewMoreBtn.addEventListener('mouseover', function() {
     viewMoreBtn.lastChild.classList.remove('fa-arrow-right');
